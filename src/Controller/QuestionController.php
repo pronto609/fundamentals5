94599c96efe93fe7ed11ab5f4e98e3e0d8f319d4
Jurij Leshchuk <ddevdev609@gmail.com>
2023-09-30 08:30:37 +0300

    add marcdown

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
-
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 class QuestionController extends AbstractController
 {
     /**
@@ -27,16 +27,19 @@ class QuestionController extends AbstractController
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdownParser)
     {
         $answers = [
-            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Make sure your cat is sitting `purrrfectly` still 🤣',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
 
+        $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+        $parceQuestionText = $markdownParser->transformMarkdown($questionText);
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
+            'questionText' => $parceQuestionText,
             'answers' => $answers,
         ]);
     }
