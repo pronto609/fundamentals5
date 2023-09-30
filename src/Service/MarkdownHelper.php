2b1ae3a497e7546585fea40f6c402df2b8670aa6
Jurij Leshchuk <ddevdev609@gmail.com>
2023-09-30 10:35:31 +0300

    service configuration

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -15,19 +15,28 @@ class MarkdownHelper
      * @var MarkdownParserInterface
      */
     private $markdownParser;
+    /**
+     * @var bool
+     */
+    private $isDebug;
 
     public function __construct(
         CacheInterface $cache,
-        MarkdownParserInterface $markdownParser
+        MarkdownParserInterface $markdownParser,
+        bool $isDebug
     ) {
         $this->cache = $cache;
         $this->markdownParser = $markdownParser;
+        $this->isDebug = $isDebug;
+        dump($this->isDebug);
     }
 
     public function parce(string $source): string
     {
-        return $this->cache->get('macdown_'.md5($source), function () use ($source) {
-            return $this->markdownParser->transformMarkdown($source);
-        });
+        if ($this->isDebug) {
+            return $this->cache->get('macdown_'.md5($source), function () use ($source) {
+                return $this->markdownParser->transformMarkdown($source);
+            });
+        }
     }
 }
